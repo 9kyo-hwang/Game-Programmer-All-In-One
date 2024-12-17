b8be618b5afe5302a1ee0698230a6a0d78073381
9kyo-hwang <ryghkd507@naver.com>
2024-12-18 01:03:52 +0900

    [Feature] TCP vs UDP

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -3,7 +3,7 @@
 
 /**
  * 소켓 생성
- * 서버에 연결 요청
+ * 서버에 연결 요청(TCP only)
  * 통신
  */
 
@@ -15,7 +15,7 @@ int main()
 		return 0;
 	}
 
-	SOCKET ClientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET ClientSocket = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (ClientSocket == INVALID_SOCKET)
 	{
 		cout << "Client Socket Create Failed :(" << endl;
@@ -29,31 +29,16 @@ int main()
 		//ServerAddress.sin_addr.S_un.S_addr = ::htonl(INADDR_ANY);  Client는 서버 주소로 맞게 접속해야 함
 		::inet_pton(AF_INET, "127.0.0.1", &ServerAddr.sin_addr);
 	}
-	if (::connect(ClientSocket, reinterpret_cast<SOCKADDR*>(&ServerAddr), sizeof(ServerAddr)) == SOCKET_ERROR)
-	{
-		cout << "Connect To Server Failed :(" << endl;
-		return 0;
-	}
 
 	while (true)
 	{
 		char SendBuffer[100] = "Hello! I am Client!";
-		int32 ResultCode = ::send(ClientSocket, SendBuffer, sizeof(SendBuffer), 0);
+		int32 ResultCode = ::sendto(ClientSocket, SendBuffer, sizeof(SendBuffer), 0, reinterpret_cast<SOCKADDR*>(&ServerAddr), sizeof(ServerAddr));
 		if (ResultCode == SOCKET_ERROR)
 		{
 			cout << "Send Client To Server Error!" << endl;
 			return 0;
 		}
-
-		char RecvBuffer[100];
-		int32 RecvLength = ::recv(ClientSocket, RecvBuffer, sizeof(RecvBuffer), 0);
-		if (RecvLength <= 0)
-		{
-			return 0;
-		}
-
-		cout << "Client Received Data: " << RecvBuffer << endl;
-		cout << "Client Received Data Length: " << RecvLength << endl;
 
 		this_thread::sleep_for(1s);
 	}
--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -3,8 +3,8 @@
 /**
  * 소켓 생성
  * 포트 bind
- * 소켓 listen
- * 클라이언트 accept
+ * 소켓 listen(TCP only)
+ * 클라이언트 accept(TCP only)
  * 통신
  */
 
@@ -24,7 +24,7 @@ int main()
 	 * return: descriptor
 	 */
 	//int32 ErrorCode = ::WSAGetLastError();
-	SOCKET ListenSocket = ::socket(/*IPv4 vs IPv6*/AF_INET, /*TCP vs UDP*/SOCK_STREAM, /*Protocol*/0);
+	SOCKET ListenSocket = ::socket(/*IPv4 vs IPv6*/AF_INET, /*TCP vs UDP*/SOCK_DGRAM, /*Protocol*/IPPROTO_UDP);
 	if (ListenSocket == INVALID_SOCKET)
 	{
 		cout << "Listen Socket Create Failed :(" << endl;
@@ -47,49 +47,20 @@ int main()
 		return 0;
 	}
 
-	/*
-	* 3. Listen
-	*/
-	if (::listen(ListenSocket, /*backlog. 대기 클라이언트 수*/SOMAXCONN) == SOCKET_ERROR)
-	{
-		cout << "Socket Listening Failed :(" << endl;
-		return 0;
-	}
-
 	while (true)
 	{
-		SOCKADDR_IN ClientAddress{};
-		int32 ClientAddressLength = sizeof(ClientAddress);
-		
-		SOCKET ClientSocket = ::accept(ListenSocket, reinterpret_cast<SOCKADDR*>(&ClientAddress), &ClientAddressLength);
-		if (ClientSocket == INVALID_SOCKET)
+		SOCKADDR_IN ClientAddr{};
+		int32 ClientAddrLen = sizeof(ClientAddr);
+
+		char RecvBuffer[100];
+		int32 RecvLength = ::recvfrom(ListenSocket, RecvBuffer, sizeof(RecvBuffer), 0, reinterpret_cast<SOCKADDR*>(&ClientAddr), &ClientAddrLen);
+		if (RecvLength <= 0)
 		{
-			cout << "Client Socket is Invalid :(" << endl;
 			return 0;
 		}
 
-		char Buffer[16];
-		::inet_ntop(AF_INET, &ClientAddress.sin_addr, Buffer, sizeof(Buffer));
-		cout << "Client Connected! IP = " << Buffer << endl;
-
-		while (true)
-		{
-			char RecvBuffer[100];
-			int32 RecvLength = ::recv(ClientSocket, RecvBuffer, sizeof(RecvBuffer), 0);
-			if (RecvLength <= 0)
-			{
-				return 0;
-			}
-
-			cout << "Server Received Data: " << RecvBuffer << endl;
-			cout << "Server Received Data Length: " << RecvLength << endl;
-
-			int32 ResultCode = ::send(ClientSocket, RecvBuffer, RecvLength, 0);
-			if (ResultCode == SOCKET_ERROR)
-			{
-				return 0;
-			}
-		}
+		cout << "Server Received Data: " << RecvBuffer << endl;
+		cout << "Server Received Data Length: " << RecvLength << endl;
 	}
 
 	::closesocket(ListenSocket);
