b5c182649e8eabeb6bf2fb4ef92a26faffc6591f
9kyo-hwang <ryghkd507@naver.com>
2024-12-19 02:57:03 +0900

    [Feature] Overlapped

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -25,14 +25,32 @@
  * 이벤트 확인: WSAEnumNetworkEvents
  */
 
+/*
+ * Overlapped IO
+ * 
+ */
+
 constexpr int32 BufferSize = 1000;
 
 struct FSession  // 서버에 있는 각자의 클라 정보
 {
+	WSAOVERLAPPED Overlapped{};  // 반드시 맨 위에 위치시켜야 함
+	// ***
 	SOCKET Socket = INVALID_SOCKET;
 	char RecvBuffer[BufferSize]{};  // 각 세션마다 RecvBuffer 필요(클라마다 보낼 데이터가 다르므로)
 	int32 RecvBytes = 0;
 };
+
+void CALLBACK RecvCallback(DWORD Error, DWORD RecvLen, LPWSAOVERLAPPED Overlapped, DWORD Flags)
+{
+	cout << "Received Data Length Callback = " << RecvLen << endl;
+
+	// TODO
+
+	// Overlapped 포인터를 넘겨받았는데, RecvLen 뿐만 아니라 Session의 나머지 데이터는 어떻게 확인할 수 있는가?
+	// Overlapped를 Session의 맨 첫 번째에 위치시켰기 때문에, Session으로 캐스팅해서 데이터를 받아올 수 있음.
+	FSession* Session = reinterpret_cast<FSession*>(Overlapped);
+}
 
 int main()
 {
@@ -62,85 +80,76 @@ int main()
 		return 0;
 	}
 
-	vector<WSAEVENT> WsaEvents;
-	vector<FSession> Sessions; Sessions.reserve(100);
+	// 
+	/**
+	 * Overlapped 함수 등록(WSARecv, WSASend)
+	 * - 성공 시 결과를 이용해 처리
+	 * - 실패 시 이유 확인
+	 */
 
-	WSAEVENT ListenEvent = ::WSACreateEvent();
-	WsaEvents.push_back(ListenEvent);
-	Sessions.push_back(FSession{ ListenSocket });  // Event - Session 1:1 매핑을 위해 더미 세션 추가
-
-	// Socket - Event 매핑. Accept, Close 이벤트를 감지할 것
-	if (::WSAEventSelect(ListenSocket, ListenEvent, FD_ACCEPT | FD_CLOSE) == SOCKET_ERROR)
-	{
-		return 0;
-	}
+	/**
+	 *
+	 */
 
 	while (true)
 	{
-		// 이벤트 배열로부터 완료된 이벤트 인덱스를 반환
-		int32 Index = ::WSAWaitForMultipleEvents(WsaEvents.size(), &WsaEvents[0], false, WSA_INFINITE, false);
-		if (Index == WSA_WAIT_FAILED)
-		{
-			continue;
-		}
+		SOCKADDR_IN ClientAddr{};
+		int32 AddrLen = sizeof(ClientAddr);
 
-		Index -= WSA_WAIT_EVENT_0;  // 보정
-
-		WSANETWORKEVENTS NetworkEvents;  // 발생한 이벤트 가져오기
-		if (::WSAEnumNetworkEvents(Sessions[Index].Socket, WsaEvents[Index], &NetworkEvents) == SOCKET_ERROR)
+		SOCKET ClientSocket;
+		while (true)
 		{
-			continue;
-		}
-
-		// Accept 이벤트가 발생했다면
-		if (NetworkEvents.lNetworkEvents & FD_ACCEPT)
-		{
-			if (NetworkEvents.iErrorCode[FD_ACCEPT_BIT] != 0)  // Error Check
+			ClientSocket = ::accept(ListenSocket, reinterpret_cast<SOCKADDR*>(&ClientAddr), &AddrLen);
+			if (ClientSocket != INVALID_SOCKET)
 			{
-				continue;
+				break;
 			}
 
-			SOCKADDR_IN ClientAddr{};
-			int32 AddrLen = sizeof(ClientAddr);
-
-			SOCKET ClientSocket = ::accept(ListenSocket, reinterpret_cast<SOCKADDR*>(&ClientAddr), &AddrLen);
-			if (ClientSocket == INVALID_SOCKET)
+			if (::WSAGetLastError() != WSAEWOULDBLOCK)
 			{
-				continue;
-			}
-
-			cout << "Client Connected" << endl;
-			WSAEVENT ClientEvent = WSACreateEvent();
-			WsaEvents.push_back(ClientEvent);
-			Sessions.push_back(FSession{ ClientSocket });
-
-			// 클라이언트가 연결됐으므로 클라이언트 소켓 - 이벤트 매핑
-			if (::WSAEventSelect(ClientSocket, ClientEvent, FD_READ | FD_WRITE | FD_CLOSE) == SOCKET_ERROR)
-			{
+				// 진짜로 문제가 있는 상황
 				return 0;
 			}
 		}
 
-		// Read 이벤트가 발생했다면 클라이언트 세션 체크
-		if (NetworkEvents.lNetworkEvents & FD_READ)
+		FSession Session
 		{
-			if (NetworkEvents.iErrorCode[FD_READ_BIT] != 0)  // Error Check
-			{
-				continue;
-			}
+			.Socket = ClientSocket,
+		};
 
-			FSession& Session = Sessions[Index];
-			int32 RecvLen = ::recv(Session.Socket, Session.RecvBuffer, BufferSize, 0);
-			if (RecvLen == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
+		cout << "Client Connected!" << endl;
+
+		// 데이터 받기
+		while (true)
+		{
+			WSABUF Buffer{};
+			Buffer.buf = Session.RecvBuffer;  // 버퍼가 날라가면 "절대 안됨"
+			Buffer.len = BufferSize;
+
+			DWORD RecvLen = 0;
+			DWORD Flags = 0;
+
+			// 한 번에 버퍼를 여러개 받을 수 있도록 설계돼있음
+			// 이제 Callback 함수를 인자로 넘겨줌
+			if (::WSARecv(ClientSocket, &Buffer, 1, &RecvLen, &Flags, &Session.Overlapped, RecvCallback) == SOCKET_ERROR)
 			{
-				if (RecvLen <= 0)
+				// 아직 Pending 상태(Send가 실제로 일어나지 않은 상태)
+				if (::WSAGetLastError() == WSA_IO_PENDING)
 				{
-					continue;
+					// Alertable Wait: 특정 이벤트가 발생하면 일어나자마자 Callback 수행
+					::SleepEx(INFINITE, true);
+
+					// C#의 경우 동일하게 콜백 함수를 인자로 받는데, 콜백 함수 실행을 위해 쓰레드 풀에서 별도의 쓰레드를 할당함
+					// C++은 별도의 쓰레드를 할당하지 않고, APC Queue에 의해 SleepEx -> Callback -> 복귀
+				}
+				else
+				{
+					// 문제가 있음
+					break;
 				}
 			}
 
-			cout << "Receive Data: " << Session.RecvBuffer << endl;
-			cout << "Receive Data Len: " << RecvLen << endl;
+			//cout << "Data Received: " << Session.RecvBuffer << endl;
 		}
 	}
 
